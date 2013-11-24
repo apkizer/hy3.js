90b7a89e5f2ef1d967218ccaa8d731fae157ae25
Alex Kizer <the.alex.kizer@gmail.com>
2013-11-24 14:03:24 -0600

    cleaning up

--- hytek.js
+++ hytek.js
@@ -4,26 +4,16 @@ var fs = require('fs'),
 var tags = JSON.parse(fs.readFileSync('tags.json').toString());
 var includeAlias = true;
 
-function is0D0A(buffer) {
-    return buffer[131] == 0x0a;
+function newline(buffer) {
+    return buffer[131] == 0x0a ? 1 : 0;
 }
 
 function parse(file) {
     var o = new Array(),
         buf = fs.readFileSync(file),
         str = buf.toString('ascii');
-    //determine if file uses 0D or 0D0A newline character, only one is interpretted by node when converted to string from buffer
-    var line = 0;
-    if (is0D0A(buf)) {
-        line = 132;
-        newline = 1;
-        console.log('newline = 0d0a')
-    } else {
-        line = 131;
-        str = str.slice(0, str.length - 1); //remove final line, because 0a is interpretted
-        console.log('newline = 0d');
-    }
-    var lines = str.length / line;
+        line = 131 + newline(buf);//determine if file uses 0D or 0D0A newline character, only one is interpretted by node when converted to string from buffer
+        lines = str.length / line;
     console.log('( ' + str.length + ' / ' + line + ') = ' + lines);
     for (var i = 0; i < lines; i++) {
         var start = i * line;
@@ -34,15 +24,6 @@ function parse(file) {
     }
     console.log(o.length);
     return o;
-}
-
-exports.parseTest1 = function (file) {
-    var buf = fs.readFileSync(file);
-    var str = buf.toString('ascii');
-    var secondLine = buf.slice(132, 132 + 130);
-    var parsed = parseLine(secondLine, tags);
-    console.log(parsed);
-
 }
 
 function parseLine(str, tags) {
@@ -62,7 +43,7 @@ function parseLine(str, tags) {
         }
         var interval = tag[intervalProperty];
         if (!interval instanceof Array || interval.length != 2) {
-            console.log('invalid interval: ' + interval);
+            return new Error('Invalid interval: ' + interval);
             continue;
         }
         var data = str.slice(interval[0], interval[1] + 1).trim(); //assuming inclusive interval
